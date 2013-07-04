74ee1d95d4195c871adf33b16cb5f0c096fc9ae4
Scott Robertson <scottymeuk@gmail.com>
2013-07-04 11:37:24 +0100

    Remove redirect in Hook, and make it "doBackground"

--- lib/CI/UI/Controller/HookController.php
+++ lib/CI/UI/Controller/HookController.php
@@ -7,12 +7,7 @@ class HookController
     {
     	$client= new \GearmanClient();
 		$client->addServer('127.0.0.1', 4730);
-		$test_id = $client->doNormal('create_test', $app['request']->get('id'));
-
-        if ($app['request']->get('redirect'))
-        {
-            return $app->redirect('/project/' . $app['request']->get('id'));
-        }
+		$test_id = $client->doBackground('create_test', $app['request']->get('id'));
 
 		return $app->json(array(
 			'success' => true
