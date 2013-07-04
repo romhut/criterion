5d671ad9e7f8c3b4f134fd1dc9f111c79e100f8b
Scott Robertson <scottymeuk@gmail.com>
2013-07-04 16:02:10 +0100

    Start working on generic hook

--- app/Criterion/UI/Controller/HookController.php
+++ app/Criterion/UI/Controller/HookController.php
@@ -5,16 +5,28 @@ class HookController
 {
     public function hook(\Silex\Application $app)
     {
-        if ($app['request']->getMethod() !== 'POST')
+        $payload = json_decode($app['request']->get('payload'));
+
+        if ( ! isset($payload['repository']['url']))
         {
             return $app->json(array(
                 'success' => false
             ));
         }
 
+        $project = $app['mongo']->projects->findOne(array(
+            'repo' => $payload['repository']['url']
+        ));
+
+        if ( ! $project)
+        {
+            $project['repo'] = $payload['repository']['url'];
+            $app['mongo']->projects->save($project);
+        }
+
     	$client= new \GearmanClient();
 		$client->addServer('127.0.0.1', 4730);
-		$test_id = $client->doBackground('create_test', $app['request']->get('id'));
+		$test_id = $client->doBackground('create_test', (string) $project['_id']);
 
 		return $app->json(array(
 			'success' => true
