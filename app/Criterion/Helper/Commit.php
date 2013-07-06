1baae535d8a0e3d13e4d6c0491b95678aa7ac40a
Scott Robertson <scottymeuk@gmail.com>
2013-07-06 10:06:27 +0100

    Add a commit helper to get URL, and branch url

---
+++ app/Criterion/Helper/Commit.php
@@ -0,0 +1,25 @@
+<?php
+namespace Criterion\Helper;
+
+class Commit
+{
+    public static function getURL($commit, $repo)
+    {
+    	if (strpos($repo, 'github.com') !== false)
+    	{
+    		return Github::commitUrl($commit, $repo);
+    	}
+
+    	return false;
+    }
+
+    public static function getBranchURL($branch, $repo)
+    {
+    	if (strpos($repo, 'github.com') !== false)
+    	{
+    		return Github::branchUrl($branch, $repo);
+    	}
+
+    	return false;
+    }
+}
