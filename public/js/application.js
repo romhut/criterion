aeffb54938c7b53c70f9946da80df82221faa5b0
Scott Robertson <scottymeuk@gmail.com>
2013-07-06 09:48:26 +0100

    Seperate JS logic out. And add a CSS theme.

---
+++ public/js/application.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+    $('.timeago').timeago();
+});
+
+function nl2br (str, is_xhtml) {
+    var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
+}
+
+var criterion = {
+    test : {
+        getStatus : function(id) {
+            $.get('/test/status/' + id, function(data) {
+
+                if (data.status.code == '1') {
+                    var status_class = 'success';
+                    clearInterval(build);
+                } else if (data.status.code == '3') {
+                    var status_class = 'warning';
+                } else if (data.status.code == '0') {
+                    var status_class = 'important';
+                } else {
+                    var status_class = 'info';
+                }
+
+                $('#status').text(data.status.message);
+                $('#status').removeClass().addClass('label').addClass('label-' + status_class);
+                if (typeof data.commit != 'undefined') {
+                    $('#commit-hash').text(data.commit.hash.short);
+                    $('#commit-author').text(data.commit.author.name);
+                    $('#branch').text(data.branch);
+                }
+
+                if (data.log.length > 0) {
+                    $('#logs').html('');
+                    $.each(data.log, function(key, val) {
+
+                        if (val.response == '0') {
+                            var hide_class = 'hide';
+                            var alert = 'success';
+                        } else {
+                            var hide_class = false;
+                            var alert = 'error';
+                        }
+
+                        $('#logs').append(
+                        '<div>' +
+                            '<a href="javascript:void(0)" class="output-log">' +
+                                '<p class="alert alert-'+alert+' ">' +
+                                    val.command +
+                                '</p>' +
+                            '</a>' +
+                            '<div class="well well-small ' + hide_class + '">' +
+                                nl2br(val.output) +
+                            '</div>' +
+                        '</div>');
+                    });
+
+                    $('.output-log').on('click', function(){
+                        $(this).next().toggle();
+                    });
+                }
+
+            });
+        }
+    }
+}
+
