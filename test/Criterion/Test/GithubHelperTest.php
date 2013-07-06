4f2dd4243c517e799e6253acf9b5530782bab896
Scott Robertson <scottymeuk@gmail.com>
2013-07-06 10:46:27 +0100

    Fully test the github helper

--- test/Criterion/Test/GithubHelperTest.php
+++ test/Criterion/Test/GithubHelperTest.php
@@ -7,9 +7,36 @@ class GithubHelperTest extends TestCase
 
     public function testSSHUrl()
     {
-        $url = 'https://github.com/romhut/api';
+        $url = 'https://github.com/romhut/criterion';
         $new_url = \Criterion\Helper\Github::toSSHUrl($url);
-        $this->assertEquals($new_url, 'git@github.com:romhut/api');
+        $this->assertEquals($new_url, 'git@github.com:romhut/criterion');
+    }
+
+    public function testHTTPSUrl()
+    {
+        $url = 'git@github.com:romhut/criterion';
+        $new_url = \Criterion\Helper\Github::toHTTPSUrl($url);
+        $this->assertEquals($new_url, 'https://github.com/romhut/criterion');
+    }
+
+    public function testCommitUrl()
+    {
+        $repo = 'git@github.com:romhut/criterion';
+        $commit = array(
+            'hash' => array(
+                'long' => '75d9fa65fdd211fd22a8c00aaf9008a1917e0f15'
+            )
+        );
+
+        $commit_url = \Criterion\Helper\Github::commitUrl($commit, $repo);
+        $this->assertEquals($commit_url, 'https://github.com/romhut/criterion/commit/75d9fa65fdd211fd22a8c00aaf9008a1917e0f15');
+    }
+
+    public function testBranchUrl()
+    {
+        $repo = 'git@github.com:romhut/criterion';
+        $commit_url = \Criterion\Helper\Github::branchUrl('master', $repo);
+        $this->assertEquals($commit_url, 'https://github.com/romhut/criterion/tree/master');
     }
 
 }
