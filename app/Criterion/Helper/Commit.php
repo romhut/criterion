b416f0a64d009186590f87508bde93a4612104a7
Scott Robertson <scottymeuk@gmail.com>
2013-07-06 10:18:17 +0100

    Tabs to Spaces

--- app/Criterion/Helper/Commit.php
+++ app/Criterion/Helper/Commit.php
@@ -5,21 +5,21 @@ class Commit
 {
     public static function getURL($commit, $repo)
     {
-    	if (strpos($repo, 'github.com') !== false)
-    	{
-    		return Github::commitUrl($commit, $repo);
-    	}
+        if (strpos($repo, 'github.com') !== false)
+        {
+            return Github::commitUrl($commit, $repo);
+        }
 
-    	return false;
+        return false;
     }
 
     public static function getBranchURL($branch, $repo)
     {
-    	if (strpos($repo, 'github.com') !== false)
-    	{
-    		return Github::branchUrl($branch, $repo);
-    	}
+        if (strpos($repo, 'github.com') !== false)
+        {
+            return Github::branchUrl($branch, $repo);
+        }
 
-    	return false;
+        return false;
     }
 }
