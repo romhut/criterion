025345099716fa40397dea35f2684c626d0c38b6
Scott Robertson <scottymeuk@gmail.com>
2013-07-07 11:05:07 +0100

    Add some failure checking tests

--- test/Criterion/Test/CommitHelperTest.php
+++ test/Criterion/Test/CommitHelperTest.php
@@ -14,13 +14,33 @@ class CommitHelperTest extends TestCase
         );
 
         $commit_url = \Criterion\Helper\Commit::getUrl($commit, $repo);
-        $this->assertEquals($commit_url, 'https://github.com/romhut/criterion/commit/75d9fa65fdd211fd22a8c00aaf9008a1917e0f15');
+        $this->assertEquals('https://github.com/romhut/criterion/commit/75d9fa65fdd211fd22a8c00aaf9008a1917e0f15', $commit_url);
+    }
+
+    public function testCommitUrlFail()
+    {
+        $repo = 'git@nonegitsite.com:romhut/criterion';
+        $commit = array(
+            'hash' => array(
+                'long' => '75d9fa65fdd211fd22a8c00aaf9008a1917e0f15'
+            )
+        );
+
+        $commit_url = \Criterion\Helper\Commit::getUrl($commit, $repo);
+        $this->assertEquals(false, $commit_url);
     }
 
     public function testBranchUrl()
     {
         $repo = 'git@github.com:romhut/criterion';
         $commit_url = \Criterion\Helper\Commit::getBranchURL('master', $repo);
-        $this->assertEquals($commit_url, 'https://github.com/romhut/criterion/tree/master');
+        $this->assertEquals('https://github.com/romhut/criterion/tree/master', $commit_url);
+    }
+
+    public function testBranchUrlFail()
+    {
+        $repo = 'git@nonegitsite.com:romhut/criterion';
+        $commit_url = \Criterion\Helper\Commit::getBranchURL('master', $repo);
+        $this->assertEquals(false, $commit_url);
     }
 }
--- test/Criterion/Test/RepoHelperTest.php
+++ test/Criterion/Test/RepoHelperTest.php
@@ -10,9 +10,30 @@ class RepoHelperTest extends TestCase
 
         $short = \Criterion\Helper\Repo::short($repo);
         $this->assertEquals('romhut/criterion', $short);
+    }
+
+    public function testShortRepoUrlFail()
+    {
+        $repo = 'https://nonegitsite.com/romhut/criterion';
+
+        $short = \Criterion\Helper\Repo::short($repo);
+        $this->assertEquals($repo, $short);
+    }
+
+    public function testProvider()
+    {
+        $repo = 'https://github.com/romhut/criterion';
 
         $provider = \Criterion\Helper\Repo::provider($repo);
         $this->assertEquals('github', $provider);
+    }
+
+    public function testProviderFail()
+    {
+        $repo = 'https://nonegitsite.com/romhut/criterion';
+
+        $provider = \Criterion\Helper\Repo::provider($repo);
+        $this->assertEquals(false, $provider);
     }
 
 }
