e8ae1385bde080d6caa04ca0745b99cc6fac0152
Marc Qualie <marc@marcqualie.com>
2013-07-13 16:43:03 +0100

    Console will now warn if dependencies are missing

--- console.php
+++ console.php
@@ -6,6 +6,15 @@ define('DATA_DIR', ROOT  . '/data');
 define('TEST_DIR', DATA_DIR . '/tests');
 define('KEY_DIR', DATA_DIR . '/keys');
 
+// Make sure system has been initialized
+if (! file_exists(__DIR__ . '/vendor/autoload.php')) {
+    echo "\n";
+    echo "  You need to install dependencies. Please run the following command:\n";
+    echo "  \033[32mcurl -sL https://getcomposer.org/installer | php && php composer.phar install\033[0m\n";
+    echo "\n";
+    exit;
+}
+
 include __DIR__ . '/vendor/autoload.php';
 $app = new Criterion\Console\Application('Criterion', '1.0');
 
