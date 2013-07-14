43c2b56c892819688c9d3c94ffc0f7ecaacc9c8c
Scott Robertson <scottymeuk@gmail.com>
2013-07-14 13:58:43 +0100

    If a project has been deleted after a test has been created, then detect this.

--- app/Criterion/Console/Command/WorkerRunCommand.php
+++ app/Criterion/Console/Command/WorkerRunCommand.php
@@ -30,6 +30,7 @@ class WorkerRunCommand extends Command
 
         $client = new \MongoMinify\Client('mongodb://127.0.0.1:27017', array('connect' => true));
         $tests = $client->criterion->tests;
+        $projects = $client->criterion->projects;
 
         while (true)
         {
@@ -48,17 +49,32 @@ class WorkerRunCommand extends Command
             if ($test)
             {
                 $output->writeln('-----------');
-                $project = (string) $test['project_id'];
-                $test = (string)  $test['_id'];
+                $project_id = (string) $test['project_id'];
+                $test_id = (string)  $test['_id'];
 
-                $output->writeln('<comment>Test Started</comment>');
-                $output->writeln(' - Project ID:' . $project);
-                $output->writeln(' - Test ID:' . $test);
+                $project = $projects->findOne(array(
+                    '_id' => $test['project_id']
+                ));
 
-                $shell_command = $input->getOption('debug') ? 'passthru' : 'shell_exec';
-                $shell_command('php ' . ROOT . '/bin/cli test ' . $test);
+                if ($project)
+                {
+                    $output->writeln('<comment>Test Started</comment>');
+                    $output->writeln(' - Project:' . $project['repo']);
+                    $output->writeln(' - Test ID:' . $test_id);
 
-                $output->writeln('<info>Done</info>');
+                    $shell_command = $input->getOption('debug') ? 'passthru' : 'shell_exec';
+                    $shell_command('php ' . ROOT . '/bin/cli test ' . $test_id);
+
+                    $output->writeln('<info>Test has finished</info>');
+                }
+                else
+                {
+                    $output->writeln('<error>Project does not exist, removing test.</error>');
+                    $tests->remove(array(
+                        '_id' => $test['_id']
+                    ));
+                }
+
                 $output->writeln('');
             }
 
