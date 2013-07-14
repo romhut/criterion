e90aef64beddd647b8211e9053ae6f207ee85255
Scott Robertson <scottymeuk@gmail.com>
2013-07-14 10:45:11 +0100

    Add users:list cli tool

---
+++ src/Criterion/Console/Command/UsersListCommand.php
@@ -0,0 +1,35 @@
+<?php
+namespace Criterion\Console\Command;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UsersListCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('users:list')
+            ->setDescription('List all users')
+            ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Check for users
+        $users = $this->getApplication()->db->selectCollection('users')->find();
+        if (! $users){
+            $output->writeln('<error>Could not find any users</error>');
+            return false;
+        }
+
+        $i = 1;
+        foreach ($users as $user)
+        {
+            echo '[' . $i . '] ' . $user['_id'] . "\n";
+            $i++;
+        }
+    }
+}
