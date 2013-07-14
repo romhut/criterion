79b642fb3ec8847702f9aa51c3a653884cc57314
Scott Robertson <scottymeuk@gmail.com>
2013-07-14 11:22:56 +0100

    Use swiftmailer for email notifications

--- src/Criterion/Helper/Notifications.php
+++ src/Criterion/Helper/Notifications.php
@@ -9,18 +9,45 @@ class Notifications
         if (isset($project['email']) && $project['email'] && isset($config['email']))
         {
             $subject = '['.$project['repo'].'] Tests Failed (' . $test . ')';
-            $message = "This is a short email to let you know that the following project's tests are failing: \n\n";
-            $message .= $config['url'] . '/test/' . $test . "\n\n";
-            $message .= 'Thanks';
+            $body = "This is a short email to let you know that the following project's tests are failing: \n\n";
+            $body .= $config['url'] . '/test/' . $test . "\n\n";
+            $body .= 'Thanks';
 
-            return self::email($project['email'], $config['email'], $subject, $message);
+            return self::email($project['email'], $config['email'], $subject, $body);
         }
         return false;
     }
 
-    private static function email($to, $from, $subject, $message)
+    private static function email($to, $from, $subject, $body)
     {
-        $headers = "From:" . $from;
-        return mail($to, $subject, $message, $headers);
+        if (isset($from['smtp']))
+        {
+            $transport = \Swift_SmtpTransport::newInstance($from['smtp']['server'], $from['smtp']['port'])
+                ->setUsername($from['smtp']['username'])
+                ->setPassword($from['smtp']['password'])
+                ;
+        }
+        else
+        {
+            $transport = \Swift_MailTransport::newInstance();
+        }
+
+        $mailer = \Swift_Mailer::newInstance($transport);
+        $message = \Swift_Message::newInstance()
+                    ->setSubject($subject)
+                    ->setFrom(array($from['address'] => $from['name']))
+                    ->setTo($to)
+                    ->setBody($body)
+                    ;
+
+        try
+        {
+            return $mailer->send($message);
+        }
+        catch (\Swift_TransportException $e)
+        {
+            echo 'Could not connect to SMTP server.' . "\n";
+            return false;
+        }
     }
 }
