60ac84d55a22a56808a794ac4ad8dcb0f7512a83
Scott Robertson <scottymeuk@gmail.com>
2013-07-16 21:50:07 +0100

    Convert models to use "username"

--- app/Criterion/Console/Command/UserCreateCommand.php
+++ app/Criterion/Console/Command/UserCreateCommand.php
@@ -23,32 +23,35 @@ class UserCreateCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-
         $username = trim(strtolower($input->getArgument('username')));
 
         // Prompt for password
         $dialog = $this->getHelperSet()->get('dialog');
 
         // Check if user exists
-        $user = new \Criterion\Model\User($username);
+        $user = new \Criterion\Model\User(array(
+            'username' => $username
+        ));
+
         if ($user->exists)
         {
             $output->writeln('<error>User already exists</error>');
             return false;
         }
 
-        $user->_id = $username;
-
-        $password = $dialog->askHiddenResponse(
+        $user->username = $username;
+        $user->password = $dialog->askHiddenResponse(
             $output,
             'Enter password: '
         );
-        if (! $password) {
+
+        if ( ! $user->password)
+        {
             $output->writeln('<error>Cannot set a blank password</error>');
             exit;
         }
-        $user->password = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
 
+        $user->password = $user->password();
         $admin = strtolower($dialog->ask($output, 'Do you want this user to be an admin? [y/N]: '));
         $user->role = 'user';
         if ($admin === 'y')
