1206089461d342116d60b9745e9bb83e43a6ecd8
Scott Robertson <scottymeuk@gmail.com>
2013-07-18 20:19:30 +0100

    Define constants, and drop DB each test setup

--- test/Criterion/Test/TestCase.php
+++ test/Criterion/Test/TestCase.php
@@ -8,6 +8,18 @@ class TestCase extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         putenv('APP_ENV=testing');
+
+        $app = new \Criterion\Application();
+        $app->db->drop();
+
+        if ( ! defined('ROOT'))
+        {
+            define('ROOT', dirname(dirname(dirname(__DIR__))));
+            define('CONFIG_FILE', ROOT . '/config.json');
+            define('DATA_DIR', ROOT  . '/data');
+            define('TEST_DIR', DATA_DIR . '/tests');
+            define('KEY_DIR', DATA_DIR . '/keys');
+        }
     }
 
 }
