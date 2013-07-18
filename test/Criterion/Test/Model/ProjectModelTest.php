608fe2392128336d0e0742334aa67dfae72430c7
Scott Robertson <scottymeuk@gmail.com>
2013-07-18 20:22:25 +0100

    Test getTests in Project Model

--- test/Criterion/Test/Model/ProjectModelTest.php
+++ test/Criterion/Test/Model/ProjectModelTest.php
@@ -19,4 +19,20 @@ class ProjectModelTest extends \Criterion\Test\TestCase
 
         $this->assertTrue(is_object($project));
     }
+
+    public function testGetTests()
+    {
+        $project = new \Criterion\Model\Project();
+        $this->assertTrue(is_object($project));
+
+        $test = new \Criterion\Model\Test();
+        $this->assertTrue(is_object($test));
+        $test->project_id = $project->id;
+        $test->save();
+
+        $tests = $project->getTests();
+
+        $this->assertTrue(is_array($tests));
+        $this->assertTrue(count($tests) === 1);
+    }
 }
