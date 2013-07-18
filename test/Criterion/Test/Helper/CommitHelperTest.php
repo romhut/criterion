13f03ead0f5b1efac69893258e1f7c83f1f85201
Scott Robertson <scottymeuk@gmail.com>
2013-07-18 20:28:22 +0100

    Test bitbucket urls

--- test/Criterion/Test/Helper/CommitHelperTest.php
+++ test/Criterion/Test/Helper/CommitHelperTest.php
@@ -4,7 +4,7 @@ namespace Criterion\Test\Helper;
 
 class CommitHelperTest extends \Criterion\Test\TestCase
 {
-    public function testCommitUrl()
+    public function testGithubCommitUrl()
     {
         $repo = 'git@github.com:romhut/criterion';
         $commit = array(
@@ -15,6 +15,19 @@ class CommitHelperTest extends \Criterion\Test\TestCase
 
         $commit_url = \Criterion\Helper\Commit::getUrl($commit, $repo);
         $this->assertEquals('https://github.com/romhut/criterion/commit/75d9fa65fdd211fd22a8c00aaf9008a1917e0f15', $commit_url);
+    }
+
+        public function testBitbucketCommitUrl()
+    {
+        $repo = 'git@bitbucket.org:romhut/criterion';
+        $commit = array(
+            'hash' => array(
+                'long' => '75d9fa65fdd211fd22a8c00aaf9008a1917e0f15'
+            )
+        );
+
+        $commit_url = \Criterion\Helper\Commit::getUrl($commit, $repo);
+        $this->assertEquals('https://bitbucket.org/romhut/criterion/commits/75d9fa65fdd211fd22a8c00aaf9008a1917e0f15', $commit_url);
     }
 
     public function testCommitUrlFail()
@@ -30,11 +43,18 @@ class CommitHelperTest extends \Criterion\Test\TestCase
         $this->assertEquals(false, $commit_url);
     }
 
-    public function testBranchUrl()
+    public function testGithubBranchUrl()
     {
         $repo = 'git@github.com:romhut/criterion';
         $commit_url = \Criterion\Helper\Commit::getBranchURL('master', $repo);
         $this->assertEquals('https://github.com/romhut/criterion/tree/master', $commit_url);
+    }
+
+    public function testBitbucketBranchUrl()
+    {
+        $repo = 'git@bitbucket.org:romhut/criterion';
+        $commit_url = \Criterion\Helper\Commit::getBranchURL('master', $repo);
+        $this->assertEquals('https://bitbucket.org/romhut/criterion/src?at=master', $commit_url);
     }
 
     public function testBranchUrlFail()
