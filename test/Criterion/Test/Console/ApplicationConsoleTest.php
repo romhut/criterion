44a2bca4abf96be0f18f7a8403df31426ecb751e
Scott Robertson <scottymeuk@gmail.com>
2013-07-21 12:11:17 +0100

    Start testing Console/Application

---
+++ test/Criterion/Test/Console/ApplicationConsoleTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Criterion\Test\Console;
+
+class ApplicationConsoleTest extends \Criterion\Test\TestCase
+{
+
+    public function testInit()
+    {
+        $app = new \Criterion\Console\Application('Criterion Testing', '0.1');
+        $this->assertTrue(is_object($app));
+        $this->assertTrue(property_exists($app, 'mongo'));
+        $this->assertTrue(property_exists($app, 'app'));
+        $this->assertTrue(property_exists($app, 'mongo'));
+    }
+
+    public function testSetAndGet()
+    {
+        $app = new \Criterion\Console\Application('Criterion Testing', '0.1');
+        $this->assertTrue(is_object($app));
+
+        $app->testing = 'test';
+        $this->assertEquals('test', $app->testing);
+    }
+
+    public function testNull()
+    {
+        $app = new \Criterion\Console\Application('Criterion Testing', '0.1');
+        $this->assertTrue(is_object($app));
+        $this->assertNull($app->testing);
+    }
+
+    public function testPreLog()
+    {
+        $app = new \Criterion\Console\Application('Criterion Testing', '0.1');
+
+        $project = new \Criterion\Model\Project();
+        $test = new \Criterion\Model\Test();
+
+        $app->project = $project;
+        $app->test = $test;
+
+        $log = $app->preLog('echo "hi";');
+        $this->assertTrue(is_object($log));
+    }
+
+    public function testLog()
+    {
+        $app = new \Criterion\Console\Application('Criterion Testing', '0.1');
+
+        $project = new \Criterion\Model\Project();
+        $test = new \Criterion\Model\Test();
+
+        $app->project = $project;
+        $app->test = $test;
+
+        $log = $app->log('echo "hi";', 'hello');
+        $this->assertTrue(is_object($log));
+        $this->assertNotNull($log->id);
+    }
+
+}
