4866d23fb6cebc7d4b806009fd4c720bfbe25bb9
Scott Robertson <scottymeuk@gmail.com>
2013-07-21 11:30:50 +0100

    100% coverage on repo helper

--- test/Criterion/Test/Helper/RepoHelperTest.php
+++ test/Criterion/Test/Helper/RepoHelperTest.php
@@ -4,9 +4,17 @@ namespace Criterion\Test\Helper;
 
 class RepoHelperTest extends \Criterion\Test\TestCase
 {
-    public function testShortRepoUrl()
+    public function testShortRepoUrlGithub()
     {
         $repo = 'https://github.com/romhut/criterion';
+
+        $short = \Criterion\Helper\Repo::short($repo);
+        $this->assertEquals('romhut/criterion', $short);
+    }
+
+    public function testShortRepoUrlBitbucket()
+    {
+        $repo = 'https://bitbucket.org/romhut/criterion';
 
         $short = \Criterion\Helper\Repo::short($repo);
         $this->assertEquals('romhut/criterion', $short);
@@ -26,6 +34,41 @@ class RepoHelperTest extends \Criterion\Test\TestCase
 
         $provider = \Criterion\Helper\Repo::provider($repo);
         $this->assertEquals('github', $provider);
+    }
+
+    public function testUsername()
+    {
+        $repo = 'git@github.com:romhut/criterion';
+
+        $username = \Criterion\Helper\Repo::username($repo);
+        $this->assertEquals('git', $username);
+    }
+
+    public function testCloneCommand()
+    {
+        $project = new \Criterion\Model\Project(array(
+            'repo' => 'git@github.com:romhut/criterion'
+        ));
+
+        $test = new \Criterion\Model\Test();
+        $test->branch = 'master';
+
+        $clone_command = \Criterion\Helper\Repo::cloneCommand($test, $project);
+        $this->assertEquals('export GIT_SSH='.ROOT.'/bin/git; export PKEY='.ROOT.'/data/keys/'.$project->id.'; git clone -b master --depth=1 git@github.com:romhut/criterion '.$test->id, $clone_command);
+    }
+
+    public function testCloneTypeHttps()
+    {
+        $repo = 'https://github.com/romhut/criterion';
+        $provider = \Criterion\Helper\Repo::cloneType($repo);
+        $this->assertEquals('https', $provider);
+    }
+
+    public function testCloneTypeSSH()
+    {
+        $repo = 'git@github.com:romhut/criterion';
+        $provider = \Criterion\Helper\Repo::cloneType($repo);
+        $this->assertEquals('ssh', $provider);
     }
 
     public function testProviderFail()
