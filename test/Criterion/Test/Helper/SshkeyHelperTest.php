69b8f001730b8af03c9eefc6267083dcf4146eba
Scott Robertson <scottymeuk@gmail.com>
2013-07-25 20:31:23 +0100

    Test the SshKey Helper

---
+++ test/Criterion/Test/Helper/SshkeyHelperTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Criterion\Test\Helper;
+
+class SshKeyHelperTest extends \Criterion\Test\TestCase
+{
+    public function testInit()
+    {
+        $key = new \Criterion\Helper\SshKey();
+        $this->assertTrue(is_object($key));
+    }
+
+    public function testGenerate()
+    {
+        $key = new \Criterion\Helper\SshKey();
+        $this->assertTrue(is_object($key));
+
+        $file = '/tmp/criterion_testing' . time();
+        $generate = $key->generate($file);
+        $this->assertTrue(!empty($generate));
+
+        $key->destroy();
+    }
+
+    public function testGenerateDirMissing()
+    {
+        $key = new \Criterion\Helper\SshKey();
+        $this->assertTrue(is_object($key));
+
+        $file = '/tmp/blah/criterion_testing' . time();
+        $generate = $key->generate($file);
+        $this->assertTrue(!empty($generate));
+
+        $key->destroy();
+    }
+
+    public function testGetPrivateKey()
+    {
+        $key = new \Criterion\Helper\SshKey();
+        $this->assertTrue(is_object($key));
+
+        $file = '/tmp/criterion_testing' . time();
+        $generate = $key->generate($file);
+        $this->assertTrue(!empty($generate));
+
+        $getPrivateKey = $key->getPrivateKey();
+        $this->assertTrue(!empty($getPrivateKey));
+
+        $key->destroy();
+    }
+
+    public function testGetPublicKey()
+    {
+        $key = new \Criterion\Helper\SshKey();
+        $this->assertTrue(is_object($key));
+
+        $file = '/tmp/criterion_testing' . time();
+        $generate = $key->generate($file);
+        $this->assertTrue(!empty($generate));
+
+        $getPublicKey = $key->getPublicKey();
+        $this->assertTrue(!empty($getPublicKey));
+
+        $key->destroy();
+    }
+
+    public function testDestroy()
+    {
+        $key = new \Criterion\Helper\SshKey();
+        $this->assertTrue(is_object($key));
+
+        $file = '/tmp/criterion_testing' . time();
+        $generate = $key->generate($file);
+        $this->assertTrue(!empty($generate));
+
+        $getPublicKey = $key->getPublicKey();
+        $this->assertTrue(!empty($getPublicKey));
+
+        $this->assertTrue($key->destroy());
+    }
+}
