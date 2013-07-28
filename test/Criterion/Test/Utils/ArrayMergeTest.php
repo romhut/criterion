47dae9a1a140de72cdd2eb3f2c2c1be852e4845d
Marc Qualie <marc@marcqualie.com>
2013-07-28 19:17:26 +0100

    Add nested array tests

--- test/Criterion/Test/Utils/ArrayMergeTest.php
+++ test/Criterion/Test/Utils/ArrayMergeTest.php
@@ -25,4 +25,35 @@ class ArrayMergeTest extends TestCase
         ));
     }
 
+
+    public function testNestedArrayMerge()
+    {
+        $array1 = array(
+            'key1' => 1,
+            'key2' => array(
+                'nested1' => 1,
+                'nested2' => 2
+            ),
+        );
+        $array2 = array(
+            'key2' => array(
+                'nested2' => 'two'
+            ),
+            'key3' => array(
+                'nested3' => 3
+            )
+        );
+        $array3 = Utils::array_merge($array1, $array2);
+        $this->assertEquals($array3, array(
+            'key1' => 1,
+            'key2' => array(
+                'nested1' => 1,
+                'nested2' => 'two'
+            ),
+            'key3' => array(
+                'nested3' => 3
+            )
+        ));
+    }
+
 }
