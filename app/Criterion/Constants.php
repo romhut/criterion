e03313b5081963104984ab7bcced8db589844274
Scott Robertson <scottymeuk@gmail.com>
2013-07-29 13:11:48 +0100

    Start converting to PSR-2 standards

---
+++ app/Criterion/Constants.php
@@ -0,0 +1,6 @@
+<?php
+define('ROOT', dirname(__DIR__));
+define('CONFIG_FILE', ROOT . '/config.json');
+define('DATA_DIR', ROOT  . '/data');
+define('TEST_DIR', DATA_DIR . '/tests');
+define('KEY_DIR', DATA_DIR . '/keys');
