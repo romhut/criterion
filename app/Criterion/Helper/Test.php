aa2fb5053776d285b8bd64f513ec6e811731c53f
Scott Robertson <scottymeuk@gmail.com>
2013-07-30 08:45:42 +0100

    Fix an issue where server side configs are ignored.
    Fix an issue where server side configs are ignored if a .criterion.yml
    config file does not exist. It will now take into account the server
    side config too when detecting test type.
    
    This allows us to set server side configs such as "setup" or "script"
    for projects that do not contain .criterion.yml files. If neither are
    set it will revert back to trying to work out if its a PHPUnit project
    with composer etc.

--- app/Criterion/Helper/Test.php
+++ app/Criterion/Helper/Test.php
@@ -3,35 +3,8 @@ namespace Criterion\Helper;
 
 class Test extends \Criterion\Helper
 {
-    public static function detectType($folder)
-    {
-        if (is_dir($folder))
-        {
-            if (self::isCriterion($folder))
-            {
-                return 'criterion';
-            }
-            elseif (self::isPHPUnit($folder))
-            {
-                return 'phpunit';
-            }
-        }
-
-        return false;
-    }
-
-    public static function isCriterion($folder)
-    {
-        return file_exists($folder . '/.criterion.yml');
-    }
-
     public static function isComposer($folder)
     {
         return file_exists($folder . '/composer.json');
-    }
-
-    public static function isPHPUnit($folder)
-    {
-        return file_exists($folder . '/phpunit.xml') || file_exists($folder . '/phpunit.xml.dist');
     }
 }
