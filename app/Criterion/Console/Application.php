05f94731b77d42118ee65c1220e31ef1e21bb820
Scott Robertson <scottymeuk@gmail.com>
2013-07-31 21:25:32 +0100

    Remove un needed code in \Criterion\Console\Application

--- app/Criterion/Console/Application.php
+++ app/Criterion/Console/Application.php
@@ -5,32 +5,5 @@ use Symfony\Component\Console\Application as SymfonyApplication;
 
 class Application extends SymfonyApplication
 {
-    public $mongo = null;
-    public $db = array();
-    public $app = array();
-    public $criterion = array();
 
-    public $data = array();
-
-    public function __construct($name, $version)
-    {
-        parent::__construct($name, $version);
-        $this->app = new \Criterion\Application();
-        $this->mongo = $this->app->mongo;
-        $this->db = $this->app->db;
-    }
-
-    public function __set($key, $val)
-    {
-        $this->data[$key] = $val;
-    }
-
-    public function __get($key)
-    {
-        if (isset($this->data[$key])) {
-            return $this->data[$key];
-        }
-
-        return null;
-    }
 }
--- test/Criterion/Test/Console/ApplicationConsoleTest.php
+++ test/Criterion/Test/Console/ApplicationConsoleTest.php
@@ -9,24 +9,5 @@ class ApplicationConsoleTest extends \Criterion\Test\TestCase
     {
         $app = new \Criterion\Console\Application('Criterion Testing', '0.1');
         $this->assertTrue(is_object($app));
-        $this->assertTrue(property_exists($app, 'mongo'));
-        $this->assertTrue(property_exists($app, 'app'));
-        $this->assertTrue(property_exists($app, 'mongo'));
-    }
-
-    public function testSetAndGet()
-    {
-        $app = new \Criterion\Console\Application('Criterion Testing', '0.1');
-        $this->assertTrue(is_object($app));
-
-        $app->testing = 'test';
-        $this->assertEquals('test', $app->testing);
-    }
-
-    public function testNull()
-    {
-        $app = new \Criterion\Console\Application('Criterion Testing', '0.1');
-        $this->assertTrue(is_object($app));
-        $this->assertNull($app->testing);
     }
 }
