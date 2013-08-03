2d4c98f9693233eb11bfcd71816f34e03ae5e6ee
Marc Qualie <marc@marcqualie.com>
2013-08-03 21:10:22 +0100

    Re-added criterion app reference to cli

--- app/Criterion/Console/Application.php
+++ app/Criterion/Console/Application.php
@@ -6,4 +6,12 @@ use Symfony\Component\Console\Application as SymfonyApplication;
 class Application extends SymfonyApplication
 {
 
+    public $app;
+
+    public function __construct($name, $version)
+    {
+        parent::__construct($name, $version);
+        $this->app = new \Criterion\Application();
+    }
+
 }
